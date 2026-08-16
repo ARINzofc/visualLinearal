4922529a794b6653de142a6574fa394eb374a8e7
ARINzofc <arinsood8@gmail.com>
2026-08-17 00:31:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import numpy as a
 from operations import vector_addition,vector_subtract,vector_multiple,vector_mag,vector_dot
 from visualizer import vector_draw
 print("Linear Algebra Visualizer")
-
-
-n=a.array([1,2])
-p=a.array([3,-5])
+A=int(input("enter the vector for A:"))
+A2=int(input("enter the vector for A:"))
+B=int(input("enter the vector for B:"))
+B2=int(input("enter the vector for B:"))
+n=a.array([A,A2])
+p=a.array([B,B2])
 d=a.dot(n,p)
 result=vector_addition(n,p)
 print("Addition",vector_addition(n,p))
