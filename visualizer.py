639f9ab18941240655c58242a6dbcbd119730d69
ARINzofc <arinsood8@gmail.com>
2026-08-17 00:32:14 +0530

    Enhance vector_draw with colors and labels

--- visualizer.py
+++ visualizer.py
@@ -1,10 +1,12 @@
 import numpy as p
 import matplotlib.pyplot as plt
 def vector_draw(a,b,c):
-    plt.quiver(0,0,a[0],a[1], angles="xy", scale_units="xy", scale=1)
-    plt.quiver(0,0,b[0],b[1], angles="xy", scale_units="xy", scale=1)
-    plt.quiver(a[0],a[1],c[0],c[1], angles="xy", scale_units="xy", scale=1)
-    plt.quiver(b[0],b[1],a[0],a[1], angles="xy", scale_units="xy", scale=1)
+    plt.quiver(0,0,a[0],a[1], angles="xy", scale_units="xy", scale=1,color="blue")
+    plt.quiver(0,0,b[0],b[1], angles="xy", scale_units="xy", scale=1,color="green")
+    plt.quiver(0,0,c[0],c[1], angles="xy", scale_units="xy", scale=1,color="red")
+    plt.text(a[0],a[1] , "A")
+    plt.text(b[0],b[1] , "B")
+    plt.text(c[0],c[1] , "A+B")
     plt.axhline(0)
     plt.axvline(0)
     plt.xlim(-8,8)
